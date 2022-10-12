86b876e289f0d4eab919cfdff861970ffc52da73
maedi laziman <maedilaziman@gmail.com>
2022-10-12 11:16:52 +0700

    initial commit

---
+++ SigmonSDK.framework/Headers/SigmonSDK.h
@@ -0,0 +1,18 @@
+//
+//  SigmonSDK.h
+//  SigmonSDK
+//
+//  Created by maedi laziman on 15/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SigmonSDK.
+FOUNDATION_EXPORT double SigmonSDKVersionNumber;
+
+//! Project version string for SigmonSDK.
+FOUNDATION_EXPORT const unsigned char SigmonSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SigmonSDK/PublicHeader.h>
+
+
